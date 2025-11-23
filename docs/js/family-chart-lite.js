dd6d4d78c9aca86bf036970e6bfe29ce55068100
jasonnlw <jason.nlw@gmail.com>
2025-11-23 17:22:46 +0000

    Update family-chart-lite.js

--- docs/js/family-chart-lite.js
+++ docs/js/family-chart-lite.js
@@ -429,7 +429,7 @@ const isMale =
   genderStr.includes("bachgen") || // boy
   genderStr.includes("dyn");       // man
 
-const fillColor = isFemale ? "#ffd6e7" : (isMale ? "#cce5ff" : "#f5f5f5");
+const fillColor = isFemale ? "#ff9651" : (isMale ? "#62caca" : "#f5f5f5");
 
   // --- Card background ---
   grp.append("rect")
